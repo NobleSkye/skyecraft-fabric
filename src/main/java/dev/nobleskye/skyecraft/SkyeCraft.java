a76e728b5ac54b0444f6f3d31aeec09413c77096
Skylar <commit@nobleskye.dev>
2024-09-29 16:45:33 -0700

    added pink garnet item and raw pink garnet

--- src/main/java/dev/nobleskye/skyecraft/SkyeCraft.java
+++ src/main/java/dev/nobleskye/skyecraft/SkyeCraft.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SkyeCraft implements ModInitializer {
-	public static final String MOD_ID = "SkyeCraft"; // Make sure this matches the ID in fabric.mod.json
+	public static final String MOD_ID = "skyecraft"; // Make sure this matches the ID in fabric.mod.json
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
